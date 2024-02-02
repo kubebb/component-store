3e43eaa1cb9159a6eb78c9ce4cbb3ba676845c8c
dayuy <973860441@qq.com>
2024-02-02 17:16:16 +0800

    chore: build docker images using GitHub Actions

--- update_base_image.sh
+++ update_base_image.sh
@@ -2,11 +2,11 @@
 set -e
 
 # build base image
-base_image="172.22.96.34/front-end/component-store:base-0.1"
-docker build --build-arg _authToken=$1 -t ${base_image} -f base.dockerfile .
+base_image="kubebb/component-store-base:main"
+docker build --build-arg -t ${base_image} -f base.dockerfile .
 docker push ${base_image}
 
 # build prod base image
-base_image_prod="172.22.96.34/front-end/component-store:base-0.1-prod"
-docker build --build-arg _authToken=$1 -t ${base_image_prod} -f pro.dockerfile .
+base_image_prod="kubebb/component-store-base-pro:main"
+docker build --build-arg -t ${base_image_prod} -f pro.dockerfile .
 docker push ${base_image_prod}
